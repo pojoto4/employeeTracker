0250428e0cb52c17990c0617f5c9636fdf6ca1a1
pojoto4 <a.shauna.deleon@gmail.com>
2025-02-16 19:34:08 -0700

    updated folders and file structure to fit project more closely

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 // Import and require Pool (node-postgres)
 // We'll be creating a Connection Pool. Read up on the benefits here: https://node-postgres.com/features/pooling
-import pg from 'pg';
+import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: 'localhost',
+  host: "localhost",
   database: process.env.DB_NAME,
   port: 5432,
 });
@@ -17,9 +17,9 @@ const pool = new Pool({
 const connectToDb = async () => {
   try {
     await pool.connect();
-    console.log('Connected to the database.');
+    console.log("Connected to the database.");
   } catch (err) {
-    console.error('Error connecting to database:', err);
+    console.error("Error connecting to database:", err);
     process.exit(1);
   }
 };
